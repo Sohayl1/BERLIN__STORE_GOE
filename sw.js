f4a36af226afa17d0bbbcdcf643862dec6779a7e
Sohayl <115431425+Sohayl1@users.noreply.github.com>
2026-01-15 08:02:56 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+﻿// sw.js - أبسط كود عشان يرضي شروط PWA
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+    // هنا ممكن تضيف كود الكاش، بس ده كفاية للتثبيت
+    e.respondWith(fetch(e.request));
+});
