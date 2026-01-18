b4f9203224a342e67f8334860020305964a8fc97
Sohayl <115431425+Sohayl1@users.noreply.github.com>
2026-01-18 13:40:52 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-﻿// ملف sw.js
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] تم التثبيت');
-});
-
-self.addEventListener('fetch', (e) => {
-    // الكود ده ضروري عشان جوجل يعتبره تطبيق
-    e.respondWith(fetch(e.request));
-});
